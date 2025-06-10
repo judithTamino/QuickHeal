dd3c929bc9ed2ef630028b7ef386ccb707dc93b6
יהודית טמינו <taminojudit@hotmail.co.il>
2025-06-10 09:52:06 +0300

    Create react project, add tailwind css in react project and added admin and fronend assets

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,3 +1,9 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import { v2 as cloudinary } from 'cloudinary';
+import Doctor from '../models/doctor.model.js';
+import jwt from 'jsonwebtoken';
+
 // Add Doctor
 const addDoctor = async (req, res) => {
   try {
@@ -5,15 +11,52 @@ const addDoctor = async (req, res) => {
     const imageFile = req.file;
 
     // checking for all data to add doctor
-    if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) 
+    if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address)
       return res.json({ success: false, msg: 'Missing Details' });
 
     // validating email format
-    
-    
+    if (!validator.isEmail(email))
+      return res.json({ success: false, msg: 'Please enter a valid email' });
+
+    // validating strong password
+    if (password.length < 8)
+      return res.json({ success: false, msg: 'Please enter a strong password' });
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctor = { name, email, image: imageUrl, password: hashedPassword, speciality, degree, experience, about, fees, address: JSON.parse(address), date: Date.now() };
+
+    const newDoctor = new Doctor(doctor);
+    await newDoctor.save();
+
+    res.status(201).json({ success: true, msg: 'Doctor Added' });
+
   } catch (error) {
     console.log(error);
+    res.json({ success: false, msg: error.message });
   }
 };
 
-export { addDoctor };
+// Admin Login
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+      const token = jwt.sign(email + password, process.env.JWT_TOKEN);
+      res.json({ success: true, token });
+    } else
+      res.json({ success: false, msg: 'Invalid admin' });
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, msg: error.message });
+  }
+}
+
+export { addDoctor, loginAdmin };
--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addDoctor } from '../controllers/admin.controller.js';
+import { addDoctor, loginAdmin } from '../controllers/admin.controller.js';
 import upload from '../middlewares/multer.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post('/', upload.single('image') ,addDoctor);
+adminRouter.post('/', upload.single('image'), addDoctor);
+adminRouter.post('/login', loginAdmin);
 
 export default adminRouter;
